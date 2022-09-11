3c91dc975b5af542953ead167f6266f15521511a
彭亚伦 <a@iy.rs>
2022-09-11 14:30:09 +0800

    add quick sort

---
+++ lesson2/src/lesson2_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { lesson2_backend } from "../../declarations/lesson2_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const arr = document.getElementById("arr").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const result = await lesson2_backend.quickSort(arr);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = result;
+
+  return false;
+});
